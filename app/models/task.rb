ce2f244f0a6c0399ee53770115e7ab1fcaa0bf27
Christina Gaitan <christina.gaitan@gmail.com>
2025-03-31 13:37:47 -0600

    add validation to tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :project
   has_one :user, through: :project
+  validates :title, presence: true
 
   scope :completed, -> { where(completed: true) }
   scope :pending, -> { where(completed: false) }
