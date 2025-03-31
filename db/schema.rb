d5defc6e88442654e333b36b861946705bf4cb94
Christina Gaitan <christina.gaitan@gmail.com>
2025-03-31 10:25:19 -0600

    generate task model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_31_161100) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_31_162411) do
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.date "start_date"
@@ -19,6 +19,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_31_161100) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_projects_on_user_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.boolean "completed"
+    t.integer "project_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +39,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_31_161100) do
   end
 
   add_foreign_key "projects", "users"
+  add_foreign_key "tasks", "projects"
 end
