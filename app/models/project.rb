de32e16b6ad02cf8c5e7eacc339d7ee00ec4efb0
Christina Gaitan <christina.gaitan@gmail.com>
2025-03-31 13:00:14 -0600

    show projects for each user

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,8 @@
 class Project < ApplicationRecord
   belongs_to :user
   has_many :tasks, dependent: :destroy
+
+  def tasks_count_by_status
+    tasks.group_by(&:completed).transform_values(&:count)
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :project
   has_one :user, through: :project
+
+  scope :completed, -> { where(completed: true) }
+  scope :pending, -> { where(completed: false) }
 end
